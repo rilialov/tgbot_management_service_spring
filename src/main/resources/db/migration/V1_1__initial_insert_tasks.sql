6635a77c93c213a79c04f8cbed0c7af29079e9f6
rilialov <ilialovrustem@gmail.com>
2022-06-02 15:57:47 +0300

    Added flyway and scripts

---
+++ src/main/resources/db/migration/V1_1__initial_insert_tasks.sql
@@ -0,0 +1,4 @@
+INSERT INTO tgbot_management.tasks (task_name, task_note)
+values
+    ('Create Service connected to telegram', 'Service must use telegram api and connect to users and management service'),
+    ('Create Service for notification', 'Service must notify about tracking');
